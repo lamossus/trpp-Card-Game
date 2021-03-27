5f23059db5d76d1717a165928762a86f87babf9f
Chetverkin Mikhail <lamossus@gmail.com>
2021-03-27 16:21:35 +0300

    First Commit

---
+++ desktop/src/ru/mirea/inbo05/project/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.mirea.inbo05.project.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.mirea.inbo05.project.StarRealms;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarRealms(), config);
+	}
+}
