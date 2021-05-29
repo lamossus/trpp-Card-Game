46520de572aa34fa593f022b2388eb1aaad12b54
Chetverkin Mikhail <lamossus@gmail.com>
2021-05-29 12:31:21 +0300

    Revert "Created game launcher and application structure started."
    This reverts commit 78f50878d98eff6941fe77802135965f60013979.

--- desktop/src/ru/mirea/inbo05/project/desktop/DesktopLauncher.java
+++ desktop/src/ru/mirea/inbo05/project/desktop/DesktopLauncher.java
@@ -1,17 +1,12 @@
 package ru.mirea.inbo05.project.desktop;
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.mirea.inbo05.project.StarRealms;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "my-gdx-game";
-		cfg.useGL30 = false;
-		cfg.width = 480;
-		cfg.height = 320;
-		new LwjglApplication(new GameApplication(), cfg);
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarRealms(), config);
 	}
 }
