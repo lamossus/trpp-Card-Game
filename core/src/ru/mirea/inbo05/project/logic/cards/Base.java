ddce281da4f51faa63aaf2f699c49de3ad0c836c
NuhkadievBS <magnushasrp@gmail.com>
2021-05-31 23:51:12 +0300

    Added play method to base

--- core/src/ru/mirea/inbo05/project/logic/cards/Base.java
+++ core/src/ru/mirea/inbo05/project/logic/cards/Base.java
@@ -1,6 +1,8 @@
 package ru.mirea.inbo05.project.logic.cards;
 
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.Align;
+import ru.mirea.inbo05.project.StarRealms;
 
 /**
  * Класс описывающий свойства карт баз.
@@ -11,8 +13,11 @@ public class Base extends Card {
     /** Является ли карта авангардом */
     private boolean isTaunt;
 
+    private CardInfo baseInfo;
+
     public Base(CardInfo cardInfo) {
         super(cardInfo);
+        this.baseInfo = cardInfo;
     }
 
     public int getHealth() {
@@ -21,5 +26,21 @@ public class Base extends Card {
 
     public boolean isTaunt() {
         return isTaunt;
+    }
+
+    @Override
+    public void play() {
+        clearListeners(); // Очистить список слушателей, чтобы разыгранную карту нельзя было разыграть ещё раз
+        remove();
+
+        setScale(0.7f);
+        int playedCards = StarRealms.playerState.playedCards.size();
+        setPosition(playedCards * getWidth() * getScaleX(), getHeight() * 0.8f, Align.bottomLeft); // Расположить карту над рукой. Надо бы сделать покрасивше
+        StarRealms.playerState.bases.add(this);
+
+        StarRealms.playerState.hand.remove(baseInfo);
+
+        StarRealms.playerState.basesGroup.addActor(this);
+        createEffectButtons();
     }
 }
