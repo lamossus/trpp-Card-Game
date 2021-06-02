a2c0497be367e77fd13e7edf040af6a322319c23
NuhkadievBS <magnushasrp@gmail.com>
2021-06-02 22:12:52 +0300

    Draw command added

--- core/src/ru/mirea/inbo05/project/logic/commands/DrawCommand.java
+++ core/src/ru/mirea/inbo05/project/logic/commands/DrawCommand.java
@@ -1,8 +1,16 @@
 package ru.mirea.inbo05.project.logic.commands;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import ru.mirea.inbo05.project.StarRealms;
+import ru.mirea.inbo05.project.logic.PlayerState;
+import ru.mirea.inbo05.project.logic.cards.Card;
+import ru.mirea.inbo05.project.logic.cards.CardInfo;
+
 public class DrawCommand implements Command{
     @Override
     public boolean execute() {
+        StarRealms.playerState.draw();
         return true;
     }
 }
