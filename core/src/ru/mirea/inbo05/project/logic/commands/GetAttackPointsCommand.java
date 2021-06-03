06f22836a71abaa1f75c0a1c72f785a4d5052029
NuhkadievBS <magnushasrp@gmail.com>
2021-06-03 09:33:50 +0300

    Made comments to Command classes

--- core/src/ru/mirea/inbo05/project/logic/commands/GetAttackPointsCommand.java
+++ core/src/ru/mirea/inbo05/project/logic/commands/GetAttackPointsCommand.java
@@ -7,6 +7,11 @@ public class GetAttackPointsCommand implements Command {
     /** Количество получаемых очков атаки*/
     private int attackPoints;
 
+
+    /**
+     * Добавление игроку очков атаки, полученных от разыгранных карт
+     *
+     */
     @Override
     public boolean execute() {
         StarRealms.setAttack(StarRealms.playerState.getAttack() + attackPoints);
